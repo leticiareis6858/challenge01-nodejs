0319ed9da4efff2ece247588ce59a61336428e2c
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2023-07-26 23:09:49 -0300

    Includes 'pet' inside 'tutor' in ClientSchema

--- src/models/Client.ts
+++ src/models/Client.ts
@@ -1,37 +1,55 @@
-const mongoose = require("mongoose");
+import { Mongoose } from "mongoose";
+//const mongoose = require('mongoose');
 
 const ClientSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-  },
-  name: {
-    type: String,
-    required: [true, "Must have a name"],
-    trim: true,
-  },
-  phone: {
-    type: String,
-    required: [true, "Must have a phone number"],
-    trim: true,
-    maxlength: [11, "Phone number can not have more than eleven digits"],
-  },
-  email: {
-    type: String,
-    required: [true, "Must have an email address"],
-    trim: true,
-  },
-  date_of_birth: {
-    type: String,
-    required: [true, "Must provide a birth date"],
-    maxlength: [8, "Birth date can not be longer than eigth digits"],
-    trim: true,
-  },
-  zip_code: {
-    typeof: Number,
-    required: [true, "Must provide a zip code"],
-    maxlength: [10, "Zip code can not be longer than ten digits"],
-    trim: true,
+  tutor: {
+    id: {
+      type: Number,
+    },
+    name: {
+      type: String,
+      required: [true, "Must have a name"],
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: [true, "Must have a phone number"],
+      trim: true,
+      maxlength: [11, "Phone number can not have more than eleven digits"],
+    },
+    email: {
+      type: String,
+      required: [true, "Must have an email address"],
+      trim: true,
+    },
+    date_of_birth: {
+      type: String,
+      required: [true, "Must provide a birth date"],
+      maxlength: [8, "Birth date can not be longer than eigth digits"],
+      trim: true,
+    },
+    zip_code: {
+      typeof: Number,
+      required: [true, "Must provide a zip code"],
+      maxlength: [10, "Zip code can not be longer than ten digits"],
+      trim: true,
+    },
+    pet: {
+      id: Number,
+      name: {
+        type: String,
+        required: [true, "Must provide a name"],
+      },
+      species: {
+        type: String,
+        required: [true, "Pet must have a specie"],
+      },
+      carry: {
+        type: String,
+        required: [true, "Must provide "],
+      },
+    },
   },
 });
 
-module.exports = mongoose.model("Client", TutorSchema);
+module.exports = mongoose.model("Client", ClientSchema);
