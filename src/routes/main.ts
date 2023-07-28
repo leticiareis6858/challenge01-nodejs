7405ad9ad9940185b5a699200629d3685df25302
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2023-07-28 15:42:30 -0300

    feat:Add routes.

---
+++ src/routes/main.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+const router = Router();
+
+import {
+    getAllTutors,
+    createTutor,
+    getTutor,
+    updateTutor,
+    deleteTutor,
+} from '../controllers/tutor';
+
+import { createPet, updatePet, deletePet } from '../controllers/pets';
+
+//tutor
+
+router.route('/tutors').get(getAllTutors); //retrieves all tutors
+
+router.route('/tutor').post(createTutor); //create a new tutor
+
+router.route('/tutor/:Id').put(updateTutor).post(getTutor); //updates a tutor
+
+router.route('/tutor/:Id').delete(deleteTutor); //deletes a tutor
+
+//pets
+
+router.route('/pet/:tutorId').post(createPet); //creates a pet and adds it to a tutor
+
+router.route('/pet/:petId/tutor/:tutorId').put(updatePet); //updates a pet´s info
+
+router.route('/pet/:petId/tutor/:tutorId').delete(deletePet); //deletes a pet from a tutor
+
+//module.exports = router;
+
+export { router as clientsRoutes };
