9f4ae9a59a6755f878095908a8d6a9069d8de17b
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2023-07-28 15:39:40 -0300

    fix: Fix 'createPet','updatePet','deletePet' export.

--- src/controllers/pets.ts
+++ src/controllers/pets.ts
@@ -3,12 +3,16 @@ import { tutor, pets } from '../models/Client';
 
 //pets:
 
-const createPet = async (req: Request, res: Response) => {
+export const createPet = async (req: Request, res: Response) => {
     const Pet = await pets.create(req.body); // done
     res.status(201).json({ Pet });
 };
 
-const updatePet = async (req: Request, res: Response, next: NextFunction) => {
+export const updatePet = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
     const { tutorId, petsId } = req.params; // done
     if (!tutor) {
         return next(
@@ -27,7 +31,11 @@ const updatePet = async (req: Request, res: Response, next: NextFunction) => {
     res.status(200).json({ Pets });
 };
 
-const deletePet = async (req: Request, res: Response, next: NextFunction) => {
+export const deletePet = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
     const { tutorId, petsId } = req.params; // done
     if (!tutor) {
         return next(
@@ -41,10 +49,4 @@ const deletePet = async (req: Request, res: Response, next: NextFunction) => {
     }
     const Pet = await pets.findByIdAndRemove({ _id: petsId });
     res.status(200).json({ Pet });
-};
-
-module.exports = {
-    createPet,
-    updatePet,
-    deletePet,
 };
