5e229071280d588ca42a6c00bdff5d727d5ba404
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2023-07-28 22:22:59 -0300

    feat:Configure 'app.ts'.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,18 @@
-import express from "express";
-
+import express from 'express';
 const app = express();
+import { clientsRoutes } from './routes/main';
+import 'dotenv/config';
+import 'express-async-errors';
 
-app.listen(3000);
+//middleware
+app.use(express.json());
+
+//route
+app.use('/api/vetclinic', clientsRoutes);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is listening on port: ${port}`);
+});
+require('./db/connect');
