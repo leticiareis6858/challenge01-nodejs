4d1c4fe6485da25c985b47cbbac862a3f89c240c
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2023-07-30 19:16:14 -0300

    fix:Remove messages attached to requires. Provide more descriptive names to the exports statements.

--- src/models/Client.ts
+++ src/models/Client.ts
@@ -4,11 +4,11 @@ const { Schema } = mongoose;
 const PetsSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'Must provide a name'],
+        required: true,
     },
     species: {
         type: String,
-        required: [true, 'Pet must have a specie'],
+        required: true,
     },
     carry: {
         type: String,
@@ -26,23 +26,23 @@ const PetsSchema = new mongoose.Schema({
 const TutorSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'Must have a name'],
+        required: true,
         trim: true,
     },
     phone: {
         type: String,
-        required: [true, 'Must have a phone number'],
+        required: true,
         trim: true,
         maxlength: [11, 'Phone number can not have more than eleven digits'],
     },
     email: {
         type: String,
-        required: [true, 'Must have an email address'],
+        required: true,
         trim: true,
     },
     date_of_birth: {
         type: String,
-        required: [true, 'Must provide a birth date'],
+        required: true,
         maxlength: [
             10,
             'Birth date can not be longer than eigth digits. E.g. 01-01-2000',
@@ -51,12 +51,12 @@ const TutorSchema = new mongoose.Schema({
     },
     zip_code: {
         type: Number,
-        required: [true, 'Must provide a zip code'],
+        required: true,
         maxlength: [10, 'Zip code can not be longer than ten digits'],
         trim: true,
     },
     pets: [{ type: Schema.Types.ObjectId, ref: 'pets' }],
 });
 
-export const tutor = mongoose.model('tutor', TutorSchema);
-export const pets = mongoose.model('pets', PetsSchema);
+export const tutorModel = mongoose.model('tutor', TutorSchema);
+export const petsModel = mongoose.model('pets', PetsSchema);
