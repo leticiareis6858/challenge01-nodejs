e53c311d19a4176d704c053b02d57a7af5126abe
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2023-07-30 19:07:05 -0300

    style:remove commented lines. fix:remove unnecessary 'getTutor'.

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -4,31 +4,28 @@ const router = Router();
 import {
     getAllTutors,
     createTutor,
-    getTutor,
     updateTutor,
     deleteTutor,
 } from '../controllers/tutor';
 
 import { createPet, updatePet, deletePet } from '../controllers/pets';
 
-//tutor
+//tutor routes
 
 router.route('/tutors').get(getAllTutors); //retrieves all tutors
 
 router.route('/tutor').post(createTutor); //create a new tutor
 
-router.route('/tutor/:id').put(updateTutor).post(getTutor); //updates a tutor
+router.route('/tutor/:id').put(updateTutor); //updates a tutor
 
 router.route('/tutor/:id').delete(deleteTutor); //deletes a tutor
 
-//pets
+//pets routes
 
 router.route('/pet/:tutorId').post(createPet); //creates a pet and adds it to a tutor
 
 router.route('/pet/:petId/tutor/:tutorId').put(updatePet); //updates a pet´s info
 
 router.route('/pet/:petId/tutor/:tutorId').delete(deletePet); //deletes a pet from a tutor
-
-//module.exports = router;
 
 export { router as clientsRoutes };
